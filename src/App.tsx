3d0cb11f2796d854b209189462175da0087eabda
Fábio <fabioalmeida132@gmail.com>
2022-07-19 23:02:01 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,173 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { Datatable, downloadPDF, ColumnProps } from 'hexa-table'
+
 
 function App() {
+
+  const data = [
+    {
+      id: 'aseasd',
+      name: 'pedro thiago joão no barquinho 123',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 2,
+      name: 'thiago',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 3,
+      name: 'joao',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 'aseasd',
+      name: 'pedro thiago joão no barquinho 123',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 2,
+      name: 'thiago',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 3,
+      name: 'joao',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 'aseasd',
+      name: 'pedro thiago joão no barquinho 123',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 2,
+      name: 'thiago',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 3,
+      name: 'joao',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 'aseasd',
+      name: 'pedro thiago joão no barquinho 123',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 2,
+      name: 'thiago',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 3,
+      name: 'joao',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 'aseasd',
+      name: 'pedro thiago joão no barquinho 123',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 2,
+      name: 'thiago',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 3,
+      name: 'joao',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 'aseasd',
+      name: 'pedro thiago joão no barquinho 123',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 2,
+      name: 'thiago',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 3,
+      name: 'joao',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 'aseasd',
+      name: 'pedro thiago joão no barquinho 123',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 2,
+      name: 'thiago',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+    {
+      id: 3,
+      name: 'joao',
+      amount: [100,'teste'],
+      date: '2022-01-01'
+    },
+  ]
+  const columns: ColumnProps[] = [
+    {
+      Header: 'First Name',
+      accessor: 'name',
+      format: "avatar",
+      type: "inline",
+      export: false,
+      sortable: true,
+      style: {
+        fontSize: "12px"
+      }
+    },
+    {
+      Header: 'Amount',
+      accessor: 'amount',
+      format: 'currency'
+    },
+    {
+      Header: 'Date',
+      accessor: 'date',
+      format: 'date'
+    }
+  ];
+
+  const footer = ['','Total','150', '5'];
+  const [size, setSize] = useState(10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={() => downloadPDF(data,columns, 'Relatório123', 'newTab', {
+        orientation: 'landscape'
+      })}>teste</button>
+      <Datatable sizes={[10,20,100]} currentSize={size} onChangeSize={(size: any) => setSize(size)} columns={columns} data={data} pageCount={1} currentPage={0} tableFooter={footer} onSelectIds={(ids: any) => console.log(ids)} />
+    </>
+  )
 }
 
 export default App;
